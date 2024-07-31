436d3ede305607a992a6da8050e5e3e25afdbcb0
new-username <jenirajmca2018@gmail.com>
2024-07-31 08:51:43 +0530

    Second Commit

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,22 @@
-import React from "react"
+// src/AboutUs.js
+import React from 'react';
+import '../About/About.css';
+import { Divider, Typography } from '@mui/material';
+import image from "../../Assets/AboutUsImages/aboutimage.jpg";
 
-function AboutUs() {
+const AboutUs = () => {
   return (
-    <div>
-      About Page
-    </div>
-  )
+    <div className="container">
+      <Typography className="title" style={{ color: "#35AFFD", fontWeight: "bold", fontFamily: "Merriweather Sans", margin: "30px", }} sx={{ fontSize: { lg: "30px", sm: "28px", xs: "20px" } }}>About Us</Typography>
+      <div className='wrap' style={{ gap: "50px", height: "100%" }}>
+        <Typography className="content" style={{ fontSize: "18px", textAlign: "justify", color: "black", }}>&nbsp;&nbsp;&nbsp;&nbsp;
+          We I Shop, situated at Thiruvarur H O, Tiruvarur, Tamil Nadu, is the home of the most useful technological devices in today's age. We deal in a wide range of computers, laptops and accessories that make the use of technology easier. We have the chance to gain a huge number of customers for ourselves by providing the best quality products and services. Prior to everything, we place top-quality products which have helped us to make a place for ourselves in the market. Our experience over the years in the industry aided us to make advancements and live up to industry norms and terms that guided us to make better progress. With uninterrupted efforts towards becoming what we want to and having an irreplaceable place in the market, has only helped us to be nothing but the best.
+        </Typography>
+
+        <img className="image" src={image} />
+      </div>
+    </div >
+  );
 };
 
 export default AboutUs;
--- src/Pages/Services/Service.js
+++ src/Pages/Services/Service.js
@@ -1,9 +1,62 @@
 import React from 'react'
+import '../Services/Service.css'
+import sellingImage from "../../Assets/Serviceimages/Selling.png";
+import webDevImage from "../../Assets/Serviceimages/web.png";
+import BillingImage from "../../Assets/Serviceimages/billing.png";
+import MobDevImage from "../../Assets/Serviceimages/web.png";
+import { Typography } from '@material-ui/core';
 
-function Service() {
+
+function ServiceCard({ title, description, image }) {
   return (
-    <div>Service</div>
+
+    <div className="service-card">
+      <img className='service-image' src={image}></img>
+      <h2 className="service-title">{title}</h2>
+      <p className="service-description">{description}</p>
+    </div>
   )
+}
+function Service() {
+  const services = [
+    {
+      title: 'Selling Products',
+      description: 'We offer a wide range of products for sale, ensuring quality and value for our customers.',
+      image: sellingImage
+    },
+    {
+      title: 'Web Development',
+      description: 'Our team provides professional web development services to create stunning and functional websites.',
+      image: webDevImage
+    },
+    {
+      title: 'Billing Software',
+      description: 'We develop billing software tailored to your business needs, making invoicing and payment tracking easy.',
+      image: BillingImage
+    },
+    {
+      title: 'Mobile App Development',
+      description: 'We specialize in mobile app development, creating user-friendly and innovative applications for various platforms.',
+      image: MobDevImage
+    }
+  ];
+
+  return (
+    <div className='main'>
+      <Typography variant='h4' style={{ fontWeight: "bold", textAlign: "center", margin: "80px", color: "#35AFFD" }}>What We Do ?</Typography>
+      <div className="services-container" >
+
+        {services.map((service, index) => (
+          <ServiceCard className="service" key={index} image={service.image} title={service.title} description={service.description} />
+        ))}
+        <div className='shadowOne'></div>
+        <div className='shadowTwo'></div>
+      </div>
+    </div>
+
+
+
+  );
 }
 
 export default Service
