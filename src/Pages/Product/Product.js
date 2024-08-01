83224b3beff6f67daea25ffb5a1a78a85e827c20
new-username <jenirajmca2018@gmail.com>
2024-08-01 17:43:15 +0530

    Styles completed

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -8,28 +8,28 @@ import Productfour from "../../Assets/ProductImages/Productfour.png";
 import Productfive from "../../Assets/ProductImages/Productfive.png";
 import Productsix from "../../Assets/ProductImages/Productsix.png";
 
-const productNameList = [ 
-  { 
+const productNameList = [
+  {
     Name: "Product One",
     image: Productfirst,
   },
-  { 
+  {
     Name: "Product Two",
     image: Productsecond,
   },
-  { 
+  {
     Name: "Product Three",
     image: Productthird,
   },
-  { 
+  {
     Name: "Product Four",
     image: Productfour,
   },
-  { 
+  {
     Name: "Product Five",
     image: Productfive,
   },
-  { 
+  {
     Name: "Product Six",
     image: Productsix,
   }
@@ -37,16 +37,19 @@ const productNameList = [
 
 function Products() {
   return (
-    <Box class="product-flexbox">
-      {productNameList.map((item, index) => (
-        <Card key={index} class="product-cardsection">
-          <CardContent>
-            <img className="img-section" src={item.image} alt="" />
-          </CardContent>
-          <Typography variant='para'>{item.Name}</Typography>
-        </Card>
-      ))}
-    </Box>
+    <div>
+      <Typography variant='h4' style={{ fontWeight: "bold", textAlign: "center", margin: "30px", color: "#35AFFD" }}>Products</Typography>
+      <Box className="product-flexbox">
+        {productNameList.map((item, index) => (
+          <Card key={index} className="product-cardsection">
+            <CardContent>
+              <img className="img-section" src={item.image} alt={item.Name} />
+            </CardContent>
+            <Typography variant='subtitle1' style={{ padding: '10px', textAlign: "center", fontWeight: "bold" }}>{item.Name}</Typography>
+          </Card>
+        ))}
+      </Box>
+    </div>
   )
 }
 
