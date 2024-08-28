994738fcd6ebb7d39da4fa22fe7bb916d4b32222
KavithaThiruppathi <kavithatp98@gmail.com>
2024-08-28 19:11:35 +0530

    Portfolio Response Updated

--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -1,10 +1,31 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from 'emailjs-com';
 import "./ContactUs.css";
 import { Box, Button, Card, TextField, Typography } from "@material-ui/core";
 
 const ContactUs = () => {
   const form = useRef();
+
+  const [mobileError, setMobileError] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const validateMobile = (value) => {
+    const mobilePattern = /[6-9]{1}[0-9]{9}/;
+    if (!mobilePattern.test(value)) {
+      setMobileError('Mobile number must start with a digit and be 10 digits long');
+    } else {
+      setMobileError('');
+    }
+  };
+
+  const validateEmail = (value) => {
+    const emailPattern = /.+@.+\..+/ 
+    if (!emailPattern.test(value)) {
+      setEmailError('Please enter a valid email address');
+    } else {
+      setEmailError('');
+    }
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -36,10 +57,16 @@ const ContactUs = () => {
               <TextField className="text-field" type="text" placeholder="Enter Your Name" name="user_name" required />
             </Box>
             <Box className="text-box">
-              <TextField className="text-field" type="mob_no" placeholder="Enter Your Mobile" name="user_mobile" required />
+              <TextField className="text-field" type="mob_no" placeholder="Enter Your Mobile" name="user_mobile" inputProps={{ maxLength: 10 }} required error={!!mobileError}
+                helperText={mobileError}
+                onChange={(e) => validateMobile(e.target.value)} 
+              />
             </Box>
             <Box className="text-box">
-              <TextField className="text-field" type="email" placeholder="Enter Your Email" name="user_email" required />
+              <TextField className="text-field" type="email" placeholder="Enter Your Email" name="user_email" required  error={!!emailError}
+                helperText={emailError}
+                onChange={(e) => validateEmail(e.target.value)}
+              />
             </Box>
             <Box className="message-box">
               <TextField className="message-field" rows={3} id="outlined-textarea" multiline name="message" placeholder="Enter Your Text Message" required />
